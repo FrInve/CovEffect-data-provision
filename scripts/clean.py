ee3e4afdc406216f1a8d0001db7f373286195cbb
FrInve <francesco1.invernici@mail.polimi.it>
2022-05-09 21:39:52 +0200

    Fix format

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,6 +1,19 @@
 # ---
 # Cleaning pipeline for CORD-19 
 #
+# jupyter:
+#   jupytext:
+#     notebook_metadata_filter: ploomber
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+#   ploomber: {}
 # ---
 
 # %%
